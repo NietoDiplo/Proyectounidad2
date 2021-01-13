768f1fff1c5ea4b5af7465a8b2674c33d0396003
NietoDiplo <jnietoovalle@gmail.com>
2021-01-12 21:14:07 -0500

    Se carga unidad 2

---
+++ Scrip.js/Scrip.js
@@ -0,0 +1,20 @@
+$(".Parrafo1").mouseover(function() {
+    $(this).css("background", "blueviolet").css("color", "darkblue").css("font-size", "18px")
+})
+$(".Parrafo1").mouseout(function() {
+    $(this).css("background", "white").css("color", "black").css("font-size", "14px")
+})
+
+$(".Parrafo2").mouseover(function() {
+    $(this).css("background", "blueviolet").css("color", "darkblue").css("font-size", "18px")
+})
+$(".Parrafo2").mouseout(function() {
+    $(this).css("background", "white").css("color", "black").css("font-size", "14px")
+})
+
+$(".Parrafo3").mouseover(function() {
+    $(this).css("background", "blueviolet").css("color", "darkblue").css("font-size", "18px")
+})
+$(".Parrafo3").mouseout(function() {
+    $(this).css("background", "white").css("color", "black").css("font-size", "14px")
+})
